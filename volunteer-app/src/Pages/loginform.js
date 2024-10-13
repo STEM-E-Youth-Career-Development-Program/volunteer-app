19095ad3e0ddab0606b912ccddc6ee111e7b3e9d
Alyssa C <63886676+alyssachou@users.noreply.github.com>
2024-10-13 15:44:50 -0400

    Update loginform.js

--- volunteer-app/src/Pages/loginform.js
+++ volunteer-app/src/Pages/loginform.js
@@ -1,20 +1,27 @@
 import "./loginform.css"
 import discordLogo from './static/discord.png'
 import Oauth from './../DiscordAuth.js'
+import { useLocation } from 'react-router-dom';
 
-const auth = new Oauth();
+
 function Login() {
+    const location = useLocation();
+    const error = location.state?.error;
+    let auth = new Oauth();
     return (
     <div className="login">
         <div className="center">
             <h2>Log in</h2>
             <form>
                 <div>
-                    <button type="submit" id="submitButton" className="button">
-                        Login with Discord
-                        <a href={auth.discord_login_url}><img src={discordLogo} alt="Smartphones"></img></a>
-                    </button>
-                    <p className="smalltext"><a href="#">Need Help?</a></p>
+                    <a href={auth.discord_login_url}>
+                        <button type="button" id="submitButton" className="button">
+                            Login with Discord
+                            <img src={discordLogo} alt="Smartphones"></img>
+                        </button>
+                    </a>
+                    <p className="smalltext"><a href="/home">Need Help?</a></p>
+                    <p className="errortext smalltext">{error}</p>
                 </div>
             </form>
         </div>
