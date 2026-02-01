9cc9bf91257828f7dfd5e72b207b9411d39f333b
rahul-n231 <165613741+rahul-n231@users.noreply.github.com>
2026-02-01 13:01:01 -0600

    Login page-login help button change
    Added help text toggle for login assistance.

--- volunteer-app/src/Pages/loginform.js
+++ volunteer-app/src/Pages/loginform.js
@@ -2,12 +2,14 @@ import "./loginform.css"
 import discordLogo from './static/discord.png'
 import Oauth from './../DiscordAuth.js'
 import { useLocation } from 'react-router-dom';
+import { useState } from "react";
 
 
 function Login() {
     const location = useLocation();
     const error = location.state?.error;
     let auth = new Oauth();
+    const [showHelp, setShowHelp] = useState(false);
     return (
     <div className="login">
         <div className="center">
@@ -20,8 +22,22 @@ function Login() {
                             <img src={discordLogo} alt="Smartphones"></img>
                         </button>
                     </a>
-                    <p className="smalltext"><a href="/home">Need Help?</a></p>
-                    <p className="errortext smalltext">{error}</p>
+                    <p
+                        className="smalltext"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => setShowHelp(!showHelp)}
+                    >
+                        Need Help?
+                        {showHelp && (
+                            <p className="smalltext">
+                                If you're having trouble logging in:
+                                <br />
+                                • Make sure you're logged into Discord  
+                                <br />
+                                • Make sure you're in the STEME Discord server  
+                            </p>
+                        )}
+                    </p>
                 </div>
             </form>
         </div>
@@ -30,3 +46,4 @@ function Login() {
 }
 
 export default Login;
+
