e1b6c7ef48722f4f7e862b4d10d720a1b77210b4
Alyssa C <63886676+alyssachou@users.noreply.github.com>
2026-03-13 15:56:14 -0400

    Update index.js

--- volunteer-app/src/index.js
+++ volunteer-app/src/index.js
@@ -60,9 +60,7 @@ export {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
 
 reportWebVitals();
